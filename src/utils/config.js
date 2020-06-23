86ef3457a65430e3bd46e288b9ca0764e9f5ac5b
Gotka Ivan <ivan.gotka@aisa.ru>
2020-06-23 20:21:07 +0300

    fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,6 @@
-export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost'}:${process.env.VUE_APP_SOCKET_PORT || '3000'}`
+//export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost'}:${process.env.VUE_APP_SOCKET_PORT || '3000'}`
+
+export const url = '';
 
 export const STORE_ACTIONS = {
     joinRoom: 'joinRoom',
